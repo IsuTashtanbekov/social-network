292e756a6b5df3d2e43305196dc1c2a99d602a29
Ислам Таштанбеков <islamtastanbekov@MacBook-Air-Islam.local>
2022-12-30 09:18:15 +0600

    first commmit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ function Header() {
+  return (
+    <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/2/21/VK.com-logo.svg'></img>
+      </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <nav className='content'>
+        <div className=''><img src='https://images.unsplash.com/photo-1617814086906-d847a8bc6fca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1442&q=80'></img></div>
+      <div><img src=''></img></div>
+      <div>My post
+        <div>New post</div>
+      </div>
+      <div>Post 1</div>
+      <div>Post 1</div>
+
+      </nav>
+  )
+}
+
+export default Profile;
